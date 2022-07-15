2e7dabe1a782cb30480c40b74f6a31566625e9ea
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-15 16:01:08 +0200

    replaced scrollview with flatlist for performance reasons

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 
 export default function App() {
   const [goalText, setGoalText] = useState("");
@@ -12,7 +12,7 @@ export default function App() {
   function addGoalHandler() {
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals,
-      goalText,
+      { text: goalText, id: Math.random().toString() },
     ]);
   }
 
@@ -30,13 +30,19 @@ export default function App() {
         />
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView >
-          {courseGoals.map((goal) =>
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          )}
-        </ScrollView>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            )
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
       </View>
     </View>
   );
