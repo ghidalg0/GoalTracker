739424da501ea17ae8ef436c08c22e6952f43cd0
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-15 16:44:12 +0200

    added visual feedback

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,22 +2,29 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 export const GoalItem = (props) => {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color:"#DDE2E6" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({pressed}) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   )
 };
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 8,
     backgroundColor: "#7b2cbf",
   },
+  pressedItem: {
+    opacity: 0.1,
+  },
   goalText: {
+    padding: 8,
     color: "#fff",
   },
 });
